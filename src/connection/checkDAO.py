9f986f63a0f0932257ee20aeeb4d818f9a5e062c
anasb <anas.bentaher@student.ehb.be>
2020-12-07 16:18:07 +0100

    checkDAO by id

---
+++ src/connection/checkDAO.py
@@ -0,0 +1,14 @@
+from connection import connect
+from Entity.business import Business
+
+
+cnx = connect()
+
+
+#Check if DAO already exists
+def checkDAOById(id):
+    query = "SELECT * FROM business WHERE id = %s;"
+    cursor = cnx.cursor()
+    count = cursor.execute(query, (id))
+    cnx.commit()
+    return bool(count != 0)
