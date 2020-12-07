5a272577b3577dec37cdcc5080d8facabdf25b0d
IsmaelAmarochi <61270555+IsmaelAmarochi@users.noreply.github.com>
2020-12-07 12:20:18 +0100

    Addes business owner class

---
+++ src/Entity/business_owner.py
@@ -0,0 +1,11 @@
+# Class for the business owner
+class Business_Owner: 
+    def __init__(self, id, firstname, lastname, date_of_birth, email, phone): 
+        self.id = id
+        self.firstname = firstname
+        self.lastname = lastname
+        self.date_of_birth = date_of_birth
+        self.email = email
+        self.phone = phone
+        
+    
